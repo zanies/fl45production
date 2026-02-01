a450553b8eee0898e35feda498364c8d1846a669
zanies <zanies79@gmail.com>
2026-02-01 21:27:21 +0100

    feat: Adjust scroll animation observer threshold and root margin for earlier triggering.

--- assets/js/modules/animations/scrollAnimations.js
+++ assets/js/modules/animations/scrollAnimations.js
@@ -1,7 +1,7 @@
 export function initScrollAnimations() {
     const observerOptions = {
-        threshold: 0.1,
-        rootMargin: '0px 0px -50px 0px'
+        threshold: 0.01,
+        rootMargin: '0px 0px -100px 0px'
     };
 
     const observer = new IntersectionObserver((entries) => {
